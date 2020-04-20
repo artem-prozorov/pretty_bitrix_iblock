a8b80c258e069d7821e0cc3554903d1c90063beb
Artem Prozorov <artem.g.prozorov@gmail.com>
2020-04-20 07:30:23 +0300

    Добавил фикстуры

---
+++ tests/Fixtures/dataobjects/enum.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'ID' => '18',
+    '~ID' => '18',
+    'PROPERTY_ID' => '18',
+    '~PROPERTY_ID' => '18',
+    'VALUE' => 'Может быть',
+    '~VALUE' => 'Может быть',
+    'DEF' => 'N',
+    '~DEF' => 'N',
+    'SORT' => '500',
+    '~SORT' => '500',
+    'XML_ID' => 'M',
+    '~XML_ID' => 'M',
+    'TMP_ID' => NULL,
+    '~TMP_ID' => NULL,
+    'EXTERNAL_ID' => 'M',
+    '~EXTERNAL_ID' => 'M',
+    'PROPERTY_NAME' => 'Тренды',
+    '~PROPERTY_NAME' => 'Тренды',
+    'PROPERTY_CODE' => 'TREND',
+    '~PROPERTY_CODE' => 'TREND',
+    'PROPERTY_SORT' => '500',
+    '~PROPERTY_SORT' => '500'
+];
